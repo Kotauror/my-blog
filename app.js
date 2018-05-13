096ce46781c20dd46fb8ad8d7ce21f44cf75882c
kotpsot <zygmunt.justyna@gmail.com>
2018-05-13 18:35:42 +0100

    Update app.js and package - prepare DB for heroku.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Like = require('./models/like.js');
 const LikesController = require('./controllers/likes.js');
 const app = express();
 var bodyParser = require('body-parser');
+const { Client } = require('pg');
 
 // var jquery = require('jquery');
 app.set('view engine', 'ejs');
@@ -22,5 +23,20 @@ require('./routes/tech-articles.js')(app);
 app.get('*', (req, res) => res.status(404).send({
   message: 'Not found.',
 }));
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 module.exports = app;
