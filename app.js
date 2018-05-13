f33aeaa11f1a7e1cf3ed7d83e9353d8b03f64768
kotpsot <zygmunt.justyna@gmail.com>
2018-05-13 21:23:27 +0100

    add favicon.

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const Like = require('./models/like.js');
 const LikesController = require('./controllers/likes.js');
 const app = express();
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
 // var jquery = require('jquery');
 app.set('view engine', 'ejs');
 
+
 // app.use(express.static(__dirname + "/public"));
 app.use("/style",express.static(__dirname + "/style")); // put jquery (and other static things like css and images) to public and load it
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
